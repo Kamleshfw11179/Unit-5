d315572584dafadeb1760ed17c98bf9d9079cc9b
Kamlesh Kumar Pradhan <88648365+Kamleshfw11179@users.noreply.github.com>
2021-11-26 01:15:56 +0530

    Update Todo.jsx

--- Assi-part-2-23-11-21/src/components/Todo.jsx
+++ Assi-part-2-23-11-21/src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import "./todo.css"
 import { useState } from "react"
+import TodoList from "./Todolist"
 import { useSelector,useDispatch } from "react-redux"
 import { addTodo,delete_todo,toggleTodo,editTodo} from "../Redux/action"
 import { nanoid } from 'nanoid'
@@ -7,8 +8,6 @@ import { nanoid } from 'nanoid'
 
 export default function Todo(){
     const [title,setTitle] = useState("")
-    const [show,setShow] = useState(false);
-    const [change,setChange] = useState("")
     const x = useSelector(store=>store.todos.data)
     const dispatch = useDispatch()
     return(
@@ -29,26 +28,12 @@ export default function Todo(){
                 ,setTitle("")}}>+</button>
         </div>
         <div>
-            {x.map((e)=>{
-                return(
-                    <div key={e.id} className="showDiv">
-                    <div className="mainData">
-                    <h1>{e.title}</h1>
-                    {e.status?<h2>Done</h2>:<h2>Not Done</h2>}
-                    </div>
-                    {show?
-                    <div className="sDiv">
-                        <input type="text" value={change} placeholder="Update data" onChange={(e)=>setChange(e.target.value)}/>
-                        <button onClick={()=>{dispatch(editTodo({id:e.id,data:change})),setShow(!show)}}>Update</button>
-                        </div>:""}
-                        <div className="updateButtons">
-                    <button onClick={()=>{dispatch(toggleTodo(e.id))}}>Toggle</button>
-                    <button onClick={()=>{dispatch(delete_todo(e.id))}}>Delete</button>
-                    <button onClick={()=>setShow(!show)}>Edit</button>
-                    </div>
-                    </div>
-                )
-            })}</div>
+        {x.map((e)=>{
+            return(
+                <TodoList e={e}/>
+            )
+        })}
+            </div>
         </>
     )
 }
